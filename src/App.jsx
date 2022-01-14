2089e73136dd75cb4b40a30030f7646dd6874917
Andres <andres.ghione@gmail.com>
2022-01-14 18:56:23 -0300

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,11 @@ function App() {
     }
   }
 
-  let producto1 = new Producto("Heladera", "Es muy blanca", 2567.0)
-  let producto2 = new Producto("Cocina", "No consume nada !", 1567.0)
-  let producto3 = new Producto("Horno", "Cocina practicamente solo", 908.0)
-  let producto4 = new Producto("Mesa", "Hasta entra la suegra", 1278.0)
-  let producto5 = new Producto("Lampara", "Empeza a ver en tu casa !", 293.0)
-  let producto6 = new Producto("Silla", "No engordes mucho !", 376.0)
-
-  let productos = [producto1, producto2, producto3, producto4, producto5, producto6]
-
   return (
     <div className="App">
       <Navbar />
       <ItemListContainer
         greeting="Bienvenido a TUTI-TIENDA !"
-        items={productos}
       />
     </div>
   );
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -7,11 +7,6 @@ function ItemListContainer({ greeting, productos }) {
     <Container className="bg-light border" fluid>
       <p className="my-5 h3">{greeting}</p>
       <CardGroup>
-        { 
-            productos.forEach(element => {
-                alert('Holis')
-            })
-        }
       </CardGroup>
     </Container>
   );
